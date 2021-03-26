61164b46d0139a6fc721963fc98075a48c21c065
nguyenvanquang1210 <80742262+nguyenvanquang1210@users.noreply.github.com>
2021-03-26 16:33:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <div id="navigation">
+    <nav id="topmenu">
+        <ul>
+          <li><a href="#">Trang Chủ</a></li>
+          <li><a href="#">Thông Tin </a></li>
+          <li><a href="#">Mặt Hàng</a></li>
+          <li><a href="#">Số Lượng</a></li>
+          <li><a href="#">Đăng ký</a></li>
+        </ul>
+      </nav>
+    </div id ="header"> 
+    <P> <img src="logo.png" >
+        </P>
+   
+        <section>
+          <article>
+            <h1> Mô Hình Logo Mixi</h1>
+            <img src="lego1.jpg" alt="">
+            <div class="price"> $350.000 </div>
+            <ul>
+              <li> Thông Tin Về Lego </li>
+              <li> Lựa Chọn Màu Sắc </li>
+              <li> Cách Lắp Ghép Lego </li>
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+          <article>
+            <h1> Áo Phông Mixi</h1>
+            <img src="ao phong.jpg" alt="">
+            <div class="price"> $300.000 </div>
+            <ul>
+              <li> Thông Tin Về Áo Phông </li>
+              <li> Lựa Chọn Màu Sắc </li>
+              <li> Lựa Chọn Size </li>
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+          <article>
+            <h1> Áo 3 Lỗ Mixi</h1>
+            <img src="ao 3lo.jpg" alt="">
+            <div class="price"> $320.000 </div>
+            <ul>
+              <li> Thông Tin Về Áo 3 Lỗ </li>
+              <li> Lựa Chọn Màu Sắc </li>
+              <li> Lựa Chọn Size </li>
+            </ul>
+            <button> Đánh Giá Sản Phẩm</button>
+          </article>
+        </section>
+    
